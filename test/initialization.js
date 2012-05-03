c6da513644e7efa75b85a222dc42c33fc43b95b7
Damon Oehlman <damon.oehlman@sidelab.com>
2012-05-03 15:06:00 +1000

    Refactoring to include nopt and cli app helpers

---
+++ test/initialization.js
@@ -0,0 +1,13 @@
+var scaffolder = require('../'),
+    path = require('path'),
+    expect = require('expect.js'),
+    srcPath = path.resolve(__dirname, '..');
+
+describe('scaffolder initialization tests', function() {
+    it('should be able to detect the package path for the current app', function(done) {
+        scaffolder(module).on('ready', function() {
+            expect(this.srcPath).to.equal(srcPath);
+            done();
+        });
+    });
+});
