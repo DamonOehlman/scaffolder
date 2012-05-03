d9dd5a24976439886051c72e3d1700c4d1476762
Damon Oehlman <damon.oehlman@sidelab.com>
2012-05-03 21:07:41 +1000

    Action loading tests

--- test/actions.js
+++ test/actions.js
@@ -1,8 +1,11 @@
 var scaffolder = require('../'),
     path = require('path'),
     expect = require('expect.js'),
+    _ = require('underscore'),
     srcPath = path.resolve(__dirname, '..'),
     scaffolderOpts = {
+        argv: [],
+        silent: true,
         actionPath: path.resolve(__dirname, 'test-actions')
     };
 
@@ -12,6 +15,13 @@ describe('scaffolder action loader tests', function() {
             expect(this.actions).to.be.ok();
             expect(this.actions.test).to.be.ok();
             done();
+        });
+    });
+    
+    it('should be able to run the test action', function(done) {
+        scaffolder(_.extend({}, scaffolderOpts, { commands: ['test'] })).on('done', function(err, results) {
+            expect(results).to.contain('ok');
+            done(err);
         });
     });
 });
--- test/test-actions/test.js
+++ test/test-actions/test.js
@@ -1,1 +1,4 @@
 exports.description = 'A test action';
+exports.run = function(opts, callback) {
+    callback(null, 'ok');
+};
