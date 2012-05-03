a05a76ea0d74855fb7afe0f86d2b8f16afcdfbb1
Damon Oehlman <damon.oehlman@sidelab.com>
2012-05-03 21:09:07 +1000

    Test fixes

--- test/initialization.js
+++ test/initialization.js
@@ -1,11 +1,15 @@
 var scaffolder = require('../'),
     path = require('path'),
     expect = require('expect.js'),
-    srcPath = path.resolve(__dirname, '..');
+    srcPath = path.resolve(__dirname, '..'),
+    scaffolderOpts = {
+        argv: [],
+        silent: true
+    };
 
 describe('scaffolder initialization tests', function() {
     it('should be able to detect the package path for the current app', function(done) {
-        scaffolder().on('ready', function() {
+        scaffolder(scaffolderOpts).on('ready', function() {
             expect(this.srcPath).to.equal(srcPath);
             done();
         });
