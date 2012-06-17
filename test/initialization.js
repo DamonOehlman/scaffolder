28fd2b76bc225bf1dfe25cf89ffb08cea5da0611
Damon Oehlman <damon.oehlman@sidelab.com>
2012-06-17 20:45:08 +1000

    Added mapping of opts to the scaffolder object

--- test/initialization.js
+++ test/initialization.js
@@ -5,12 +5,33 @@ var scaffolder = require('../'),
     scaffolderOpts = {
         argv: [],
         silent: true
-    };
+    },
+    _ = require('underscore');
 
 describe('scaffolder initialization tests', function() {
     it('should be able to detect the package path for the current app', function(done) {
         scaffolder(scaffolderOpts).on('ready', function() {
             expect(this.srcPath).to.equal(srcPath);
+            done();
+        });
+    });
+    
+    it('should be able to map an object onto the scaffolder', function(done) {
+        var s = scaffolder(_.extend({ test: true }, scaffolderOpts));
+        s.on('ready', function() {
+            expect(this.test).to.equal(true);
+            done();
+        });
+    });
+    
+    it('should be able to map an function onto the scaffolder', function(done) {
+        var s = scaffolder(_.extend({
+            test: function() { return true; }
+        }, scaffolderOpts));
+        
+        s.on('ready', function() {
+            expect(typeof this.test).to.equal('function');
+            expect(this.test()).to.equal(true);
             done();
         });
     });
