890fe1b3dfff746e6b197c6de5117c4de83bac1c
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-25 13:10:59 +1100

    whitespace

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var coreCommands = {
     'help': Boolean,
     'version': Boolean
   },
-  
+
   shorthand: {}
 };
 
@@ -125,13 +125,13 @@ var coreCommands = {
   require('scaffolder')({
     // an array of default command strings that will attempt to
     // be invoked if no options are provided
-    defaultCommands: [],  
+    defaultCommands: [],
 
-    // provide any default args that individual commands 
+    // provide any default args that individual commands
     // will incorporate / overwrite
     defaultArgs: {},
 
-    // default nopt shorthand options 
+    // default nopt shorthand options
     defaultShorthand: {},
 
     // the path the scaffolder commands will be loaded from
@@ -167,22 +167,22 @@ var coreCommands = {
 **/
 function Scaffolder(opts) {
   var scaffolder = this;
-  
+
   // ensure we have options
   opts = opts || {};
-  
+
   // initialise the empty commands array
   this.commands = {};
-  
+
   this.targetModule = opts.module;
   this.ready = false;
-  
+
   // initialise the squirrel allowInstall option to 'prompt'
   opts.allowInstall = opts.allowInstall || 'prompt';
-  
+
   // save the opts for passing through to other objects
   this.opts = opts;
-  
+
   // if we are not running in silent mode, then handle errors by
   // pushing them to output
   if (! opts.silent) {
@@ -190,10 +190,10 @@ function Scaffolder(opts) {
       out('!{red}{0}', err);
     });
   }
-  
+
   // create the out helper function
   this.out = opts.silent ? function() {} : out;
-  
+
   // map the opts to the scaffolder which can allow objects to access
   _.difference(Object.keys(opts), ['init']).forEach(function(key) {
     // only map if it doesn't already exist in the object or the prototype
@@ -201,7 +201,7 @@ function Scaffolder(opts) {
       scaffolder[key] = opts[key];
     }
   });
-  
+
   // if we have a target module, then find the package
   if (this.targetModule) {
     debug('looking for package.json, starting: ' + this.targetModule.filename);
@@ -215,24 +215,24 @@ function Scaffolder(opts) {
       initializers = initializers.map(function(initializer) {
         return initializer.bind(scaffolder, opts);
       });
-      
+
       // if we encountered an error, then emit an error event and
       // abort processing
       if (err) {
         return scaffolder.emit('error', err);
       }
-      
+
       // initialise the default asset path
       scaffolder.assetPath = opts.assetPath ||
         path.resolve(srcPath, 'assets');
-      
+
       // initialise the default command path
       scaffolder.commandPath = opts.commandPath ||
         path.resolve(srcPath, 'commands');
-      
+
       // initialise the scaffolder srcPath
       scaffolder.srcPath = srcPath;
-      
+
       // load the package info
       async.parallel(initializers, function(err) {
         if (! err) {
@@ -256,7 +256,7 @@ util.inherits(Scaffolder, events.EventEmitter);
 **/
 Scaffolder.prototype.getPath = function(callback) {
   var scaffolder = this;
-  
+
   if (! this.ready) {
     this.once('ready', function() {
       scaffolder.getPath(callback);
@@ -275,22 +275,22 @@ Scaffolder.prototype.loadActions = function(opts, callback) {
   // determine the action path
   var commandPath = path.resolve(this.srcPath, this.commandPath);
   var scaffolder = this;
-  
+
   // read the files in the action path
   debug('attempting to load action definitions from: ' + commandPath);
   fs.readdir(commandPath, function(err, files) {
     var command;
     var reqErr;
     var commandModule;
-    
+
     (files || []).filter(filters.jsOnly).forEach(function(file) {
       // initialise the action module path
       commandModule = path.join(commandPath, file);
       debug('attempting to load action module: ' + commandModule);
-      
+
       // reset the action
       command = null;
-      
+
       // load the action module
       try {
         command = require(commandModule);
@@ -306,7 +306,7 @@ Scaffolder.prototype.loadActions = function(opts, callback) {
         scaffolder.commands[command.name] = command;
       }
     });
-    
+
     callback(reqErr);
   });
 };
@@ -336,7 +336,7 @@ Scaffolder.prototype.loadPackage = function(opts, callback) {
         err = new Error('Unable to parse package.json');
       }
     }
-    
+
     callback(err, scaffolder.packageData);
   });
 };
@@ -347,47 +347,47 @@ Scaffolder.prototype.loadPackage = function(opts, callback) {
 Scaffolder.prototype.main = function(opts, handler) {
   var scaffolder = this;
   var runCommand = true;
-  
+
   // if we have no arguments, then check the command line args
   if (typeof opts == 'function') {
     handler = opts;
     opts = {};
   }
-  
+
   // create a copy of the opts so changes don't get pushed back
   // to the original object
   opts = _.clone(opts);
-  
+
   // get the commands list
   opts.commands = opts.commands || (opts.argv || {}).remain || [];
-  
+
   // if commands is not an array, then make it one
   if (! Array.isArray(opts.commands)) {
     opts.commands = [opts.commands];
   }
-  
+
   // filter out missing commands
   opts.commands = (opts.commands || []).filter(function(command) {
     return scaffolder.commands[command];
   });
-  
+
   // use the default commands if we have not parsed out valid commands
   if (opts.commands && opts.commands.length === 0 && (! opts.help)) {
     debug('no commands specified, using defaults: ', opts.defaultCommands);
-    
+
     // return only default commands that actually exist
     // i.e. don't attempt to run the "run" command if no handler is defined
     opts.commands = (opts.defaultCommands || []).filter(function(command) {
       return scaffolder.commands[command];
     });
   }
-  
+
   debug('handler = ' + handler + ', help = ' + opts.help +
     ', version = ' + opts.version + ', commands to run: ', opts.commands);
 
   // if we have no commands and no handler, then set the help flag
   opts.help = opts.help || (opts.commands.length === 0 && (! handler));
-  
+
   // if we have been provided a specific handler then run that
   if (handler) {
     handler.call(scaffolder, opts, function(err) {
@@ -406,12 +406,12 @@ Scaffolder.prototype.main = function(opts, handler) {
 
           scaffolder.emit(err ? 'error' : 'done', err);
         });
-        
+
         // set the run command flag to false
         runCommand = false;
       }
     });
-    
+
     if (runCommand) {
       debug('running commands: ', opts.commands);
       async.mapSeries(
@@ -451,7 +451,7 @@ Scaffolder.prototype.read = function(prompts, callback) {
 **/
 Scaffolder.prototype.run = function(name, opts, callback) {
   var command = this.commands[name];
-  
+
   // remap args if required
   if (typeof opts == 'function') {
     callback = opts;
@@ -463,7 +463,7 @@ Scaffolder.prototype.run = function(name, opts, callback) {
     callback(new Error('Unable to find handler for command: ' + name));
     return;
   }
-  
+
   // if the action does not have a run function, then return an error
   if (typeof command.run != 'function') {
     callback(new Error('Action "' + name + '" does not have a run function'));
@@ -477,10 +477,10 @@ Scaffolder.prototype.run = function(name, opts, callback) {
     opts.argv,
     opts.startArg
   ));
-  
+
   // remove handled commands from the list
   opts.argv.remain = _.difference(opts.argv.remain, Object.keys(this.commands));
-  
+
   // run the action handler
   command.run.call(this, opts, callback);
 };
@@ -509,16 +509,16 @@ pluginFiles.forEach(function(pluginFile) {
 
 exports = module.exports = function(opts, initFn) {
   var scaffolder;
-  
+
   if (typeof opts == 'function') {
     initFn = opts;
     opts = {};
   }
-  
+
   // ensure we have options
   opts = opts || {};
-  
-  // initialise the start arg to 0 if custom args have been provided, 
+
+  // initialise the start arg to 0 if custom args have been provided,
   // or 2 if we are using process.argv
   opts.startArg = opts.argv ? 0 : 2;
 
@@ -532,7 +532,7 @@ exports = module.exports = function(opts, initFn) {
 
   opts.defaultShorthand = _.extend(opts.defaultShorthand || {},
     coreCommands.shorthand);
-  
+
   // extend the opts by parsing with nopt
   if (typeof opts.parseOpts == 'undefined' || opts.parseOpts) {
     opts = _.extend({}, opts, nopt(
@@ -540,20 +540,20 @@ exports = module.exports = function(opts, initFn) {
       opts.defaultShorthand,
       opts.argv, opts.startArg));
   }
-  
+
   // initialise the module of the options
   opts.module = opts.module || module.parent;
-  
+
   // initialise the default commands to include 'run'
   opts.defaultCommands = opts.defaultCommands || ['run'];
-  
+
   // create the scaffolder
   scaffolder = new Scaffolder(opts);
 
   // once the scaffolder is ready, run the main entry point
   scaffolder.once('ready', function() {
     debug('scaffolder ready, running main entry point');
-    
+
     // run the main function
     // if an init function has been passed, that will run
     // otherwise commands will be run if they are specified on the cli
@@ -561,6 +561,6 @@ exports = module.exports = function(opts, initFn) {
       scaffolder.main(opts, initFn);
     }
   });
-  
+
   return scaffolder;
 };
